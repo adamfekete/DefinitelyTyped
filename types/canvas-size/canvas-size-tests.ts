d1a6e00d7283c5cac82c0b897ddcca90cc433560
Adam Fekete <adam.fekete@shapr3d.com>
2024-10-30 08:57:09 +0100

    Add definitions for canvas-size 2.0.0

--- types/canvas-size/canvas-size-tests.ts
+++ types/canvas-size/canvas-size-tests.ts
@@ -1,12 +1,8 @@
-import canvasSize = require("canvas-size");
+import canvasSize from "canvas-size";
 
 const noop = () => void 0;
 
-canvasSize.test({ width: 100, height: 100 }); // $ExpectType boolean
-canvasSize.maxArea({ max: 1024, onSuccess: noop, onError: noop }); // $ExpectType void
-canvasSize.maxHeight({ min: 1, step: 64, usePromise: false }); // $ExpectType void
-canvasSize.maxHeight({ usePromise: true }); // $ExpectType Promise<CanvasSizeMaxReturn>
-// $ExpectType Promise<null>
-canvasSize.maxHeight({ max: 16384, min: 1, step: 1024, usePromise: true, useWorker: true })
-    .then(({ width, height, benchmark }) => null)
-    .catch(({ width, height, benchmark }) => null);
+canvasSize.test({ width: 100, height: 100 }); // $ExpectType Promise<CanvasSizeReturn>
+canvasSize.maxArea({ max: 1024, onSuccess: noop, onError: noop }); // $ExpectType Promise<CanvasSizeReturn>
+canvasSize.maxHeight({ min: 1, step: 64 }); // $ExpectType Promise<CanvasSizeReturn>
+canvasSize.maxWidth({ max: 1000, step: 64 }); // $ExpectType Promise<CanvasSizeReturn>
--- types/canvas-size/index.d.ts
+++ types/canvas-size/index.d.ts
@@ -1,41 +1,38 @@
-interface CanvasSizeCommonOptions {
-    useWorker?: boolean;
-    usePromise?: boolean;
-    onError?: (width: number, height: number, benchmark: number) => any;
-    onSuccess?: (width: number, height: number, benchmark: number) => any;
+declare module "canvas-size" {
+    type CanvasSizeCallbackParams = {
+        width: number;
+        height: number;
+        testTime: number;
+        totalTime: number;
+    };
+
+    type CanvasSizeOptions = {
+        max?: number;
+        min?: number;
+        step?: number;
+        useWorker?: boolean;
+        onError?: (options: CanvasSizeCallbackParams) => unknown;
+        onSuccess?: (options: CanvasSizeCallbackParams) => unknown;
+    };
+
+    type CanvasSizeReturn = CanvasSizeCallbackParams & { success: boolean };
+
+    type CanvasSizeTestOptions = {
+        width?: number;
+        height?: number;
+        sizes?: number[][];
+        useWorker?: boolean;
+        onError?: (options: CanvasSizeCallbackParams) => unknown;
+        onSuccess?: (options: CanvasSizeCallbackParams) => unknown;
+    };
+
+    type CanvasSize = {
+        maxArea(options?: CanvasSizeOptions): Promise<CanvasSizeReturn>;
+        maxHeight(options?: CanvasSizeOptions): Promise<CanvasSizeReturn>;
+        maxWidth(options?: CanvasSizeOptions): Promise<CanvasSizeReturn>;
+        test(options?: CanvasSizeTestOptions): Promise<CanvasSizeReturn>;
+    };
+
+    const canvasSize: CanvasSize;
+    export default canvasSize;
 }
-
-interface CanvasSizeTestOptions extends CanvasSizeCommonOptions {
-    width?: number;
-    height?: number;
-    sizes?: Array<[number, number]>;
-}
-
-interface CanvasSizeMaxOptions extends CanvasSizeCommonOptions {
-    max?: number;
-    min?: number;
-    step?: number;
-}
-
-interface CanvasSizeMaxReturn {
-    width: number;
-    height: number;
-    benchmark: number;
-}
-
-interface CanvasSize {
-    test(options: CanvasSizeTestOptions): boolean;
-
-    maxArea(options: CanvasSizeMaxOptions & { usePromise?: false }): void;
-    maxArea(options: CanvasSizeMaxOptions & { usePromise: true }): Promise<CanvasSizeMaxReturn>;
-
-    maxWidth(options: CanvasSizeMaxOptions & { usePromise?: false }): void;
-    maxWidth(options: CanvasSizeMaxOptions & { usePromise: true }): Promise<CanvasSizeMaxReturn>;
-
-    maxHeight(options: CanvasSizeMaxOptions & { usePromise?: false }): void;
-    maxHeight(options: CanvasSizeMaxOptions & { usePromise: true }): Promise<CanvasSizeMaxReturn>;
-}
-
-declare const canvasSize: CanvasSize;
-export as namespace canvasSize;
-export = canvasSize;
